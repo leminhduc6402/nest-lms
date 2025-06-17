978c38a6446eed5ceb929ed33a4e612cc4e2d758
leminhduc6402 <leminhduc6402@gmail.com>
2025-06-17 16:13:34 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,14 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { TransformInterceptor } from './core/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const reflector = app.get(Reflector);
   app.useGlobalGuards(new JwtAuthGuard(reflector));
-  // app.useGlobalInterceptors(new TransformInterceptor(reflector));
+  app.useGlobalInterceptors(new TransformInterceptor(reflector));
 
   app.useGlobalPipes(
     new ValidationPipe({
